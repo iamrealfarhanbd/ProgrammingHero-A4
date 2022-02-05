bc65752ecb6a4229a0f26efbdc4a19295ff13296
unknown <iamfarhan09@gmail.com>
2022-02-05 18:47:58 +0600

    solved all

--- problemSolving.js
+++ problemSolving.js
@@ -4,84 +4,84 @@
 =================================== */ 
 
 function anaToVori(ana){
-        if((typeof ana != 'number') || ana <= 0){
-            console.log("Please Insert Valid Input to check Vor")
-            return false;
-        }
-        else if(ana == 0 || ana >= 0 ){
-            result = ana * 0.0625
-            // console.log(result);
-        }
-        else{
-            console.log("Please Insert Valid Input to check Vor")
-            return false;
-        }
-        return result;
-        
+    if((typeof ana != 'number') || ana <= 0){
+        console.log("Please Insert Valid Input to check Vor")
+        return false;
+    }
+    else if(ana == 0 || ana >= 0 ){
+        result = ana * 0.0625
+        // console.log(result);
+    }
+    else{
+        console.log("Please Insert Valid Input to check Vor")
+        return false;
+    }
+    return result;
+    
 }
 const vori = anaToVori(16);
 // console.log(vori)
 
 /* ================================
-         Task 2 : pandaCost
+     Task 2 : pandaCost
 =================================== */
 function pandaCost(shingara,shomocha,zilapi){
-    if((typeof shingara === 'number' && typeof shomocha === 'number' && typeof zilapi === 'number') && (shingara >=0 && shomocha >=0 && zilapi >=0) ){
+if((typeof shingara === 'number' && typeof shomocha === 'number' && typeof zilapi === 'number') && (shingara >=0 && shomocha >=0 && zilapi >=0) ){
 
-        shingaraTotal = shingara * 7;
-        shomochaTotal = shomocha * 10;
-        zilapiTotal = zilapi * 15;
-         
-        Total = shingaraTotal + shomochaTotal + zilapiTotal;
+    shingaraTotal = shingara * 7;
+    shomochaTotal = shomocha * 10;
+    zilapiTotal = zilapi * 15;
+     
+    Total = shingaraTotal + shomochaTotal + zilapiTotal;
     
-        // console.log(Total)
-        return Total;
-    }
-    else if(shingara <=0 && shomocha <=0 && zilapi <=0){
-        console.log("Please Insert Valid Input to check pandaCost")
-        return false;
-    } else{
-        console.log("Please Insert Valid Input to check pandaCost")
-        return false;
-     }
+    // console.log(Total)
+}
+else if(shingara <=0 && shomocha <=0 && zilapi <=0){
+    console.log("Please Insert Valid Input to check pandaCost")
+    return false;
+} else{
+    console.log("Please Insert Valid Input to check pandaCost")
+    return false;
+ }
+ return Total;
 
 }
 
 const totalOrder = pandaCost(1,1,1);
-console.log(totalOrder)
+// console.log(totalOrder)
 
 
 /* ================================
-         Task 3 : picnicBudget
+     Task 3 : picnicBudget
 =================================== */
 function picnicBudget(persons){
-    if(typeof persons != 'number' ){
-        console.log("Please Insert Valid Input to check pandaCost")
-        return false;
-    }
-    else if(persons > 0 && persons <= 100){
-        personsTotal = persons * 5000;  
-        Total = personsTotal;
-    
-        // console.log(Total)
-    }
-    else if(persons > 100 && persons <= 200){
-        personsTotal = persons * 4000;  
-        Total = personsTotal;
-    
-        // console.log(Total)
-    }
-    else if(persons >= 201){
-        personsTotal = persons * 3000;  
-        Total = personsTotal;
-    
-        // console.log(Total)
-    }
-    else{
-        console.log("Please Insert Valid Input to check pandaCost")
-        return false;
-     }
-     return Total;
+if(typeof persons != 'number' ){
+    console.log("Please Insert Valid Input to check pandaCost")
+    return false;
+}
+else if(persons > 0 && persons <= 100){
+    personsTotal = persons * 5000;  
+    Total = personsTotal;
+
+    // console.log(Total)
+}
+else if(persons > 100 && persons <= 200){
+    personsTotal = persons * 4000;  
+    Total = personsTotal;
+
+    // console.log(Total)
+}
+else if(persons >= 201){
+    personsTotal = persons * 3000;  
+    Total = personsTotal;
+
+    // console.log(Total)
+}
+else{
+    console.log("Please Insert Valid Input to check pandaCost")
+    return false;
+ }
+ return Total;
 }
 
 const totalCost = picnicBudget(201);
@@ -89,21 +89,21 @@ const totalCost = picnicBudget(201);
 
 
 /* ================================
-         Task 4 : oddFriend
+     Task 4 : oddFriend
 =================================== */
 
 function oddFriend(friendName){
-    let oddsName = '';
+let oddsName = '';
 for(friend of friendName){
-    nameOdd = friend;
-    text = nameOdd.length;
-    if(text %2 ==1){
-        oddsName = friend + oddsName;
-        break;
+nameOdd = friend;
+text = nameOdd.length;
+if(text %2 ==1){
+    oddsName = friend + oddsName;
+    break;
 
-    }
 }
-  return oddsName;
+}
+return oddsName;
 
 }
 const oddName = oddFriend(['farhan','tasnova','fairooz','mahi','razel']);
